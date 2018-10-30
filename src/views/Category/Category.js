1ef25fe588a6fa88edc215b83f1d0e782ceb2924
Marion <marion_ott@me.com>
2018-10-30 15:15:27 +0100

    code updates

--- src/views/Category/Category.js
+++ src/views/Category/Category.js
@@ -2,10 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Category = ({ categoryName, category }) => {
-  console.log(category.clues[2].question);
+  const alreadyAsked = [];
+  
   return (
     <section>
       <h1>Category page: {categoryName}</h1>
+      {category.clues.map(clue => (
+        <div>
+          <p>{clue.question}</p>
+          <input placeholder="Type your answer"></input>
+        </div>
+      ))}
     </section>
   );
 }
